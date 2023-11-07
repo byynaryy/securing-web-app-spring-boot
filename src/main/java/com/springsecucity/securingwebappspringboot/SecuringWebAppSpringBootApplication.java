04bdc3c79b9faa9e280e67ec9a9149d387ffc45b
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-07 23:06:49 +0100

    feat: initial commit

---
+++ src/main/java/com/springsecucity/securingwebappspringboot/SecuringWebAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springsecucity.securingwebappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebAppSpringBootApplication.class, args);
+	}
+
+}
