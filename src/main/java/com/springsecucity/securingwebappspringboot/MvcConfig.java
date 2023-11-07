669218cf5a65d91158ccebb63839b2c944c88cb4
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-08 00:08:38 +0100

    feat(MvcConfig.java): add implements WebMvcConfigurer

---
+++ src/main/java/com/springsecucity/securingwebappspringboot/MvcConfig.java
@@ -0,0 +1,6 @@
+package com.springsecucity.securingwebappspringboot;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+}
