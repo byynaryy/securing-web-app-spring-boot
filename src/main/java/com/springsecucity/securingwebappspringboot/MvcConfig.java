3881db32c81a40a271152266af4d01287148c6f1
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-08 00:10:16 +0100

    feat(MvcConfig.java): add addViewControllers function

--- src/main/java/com/springsecucity/securingwebappspringboot/MvcConfig.java
+++ src/main/java/com/springsecucity/securingwebappspringboot/MvcConfig.java
@@ -1,6 +1,13 @@
 package com.springsecucity.securingwebappspringboot;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
 }
