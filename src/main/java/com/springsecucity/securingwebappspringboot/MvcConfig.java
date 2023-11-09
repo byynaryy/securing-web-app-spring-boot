dceb3d01e52669b43a4815440280558a5531781a
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-09 12:34:52 +0100

    feat(securingwebappspringboot/MvcConfig.java): add Configuration annotation

--- src/main/java/com/springsecucity/securingwebappspringboot/MvcConfig.java
+++ src/main/java/com/springsecucity/securingwebappspringboot/MvcConfig.java
@@ -1,8 +1,9 @@
 package com.springsecucity.securingwebappspringboot;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
