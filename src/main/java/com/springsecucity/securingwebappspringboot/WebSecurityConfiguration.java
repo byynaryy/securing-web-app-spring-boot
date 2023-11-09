f26eb7969363a582cc4544b985ffab9348feda51
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-09 13:40:56 +0100

    feat(WebSecurityConfiguration.java): add Configuration annotation

--- src/main/java/com/springsecucity/securingwebappspringboot/WebSecurityConfiguration.java
+++ src/main/java/com/springsecucity/securingwebappspringboot/WebSecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.springsecucity.securingwebappspringboot;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
 }
